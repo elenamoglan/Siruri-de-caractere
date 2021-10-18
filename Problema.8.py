adab1103d318fb6ac9c4aa03cb93f7098e58a75e
elenamoglan <70642076+elenamoglan@users.noreply.github.com>
2021-10-19 00:01:06 +0300

    Add files via upload

---
+++ Problema.8.py
@@ -0,0 +1,10 @@
+S='A B a Z Z Y c'
+S1=''
+for i in range(len(S)):
+    if ord(S[i])==90:
+        S1+='A'
+    elif ord(S[i])==32:
+        S1+=' '
+    else:
+        S1+=chr(ord(S[i])+1)
+print(S1)
