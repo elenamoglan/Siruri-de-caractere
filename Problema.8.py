f913143c50d168770a8b7ec1fc046957c94b0781
elenamoglan <70642076+elenamoglan@users.noreply.github.com>
2021-10-19 17:00:03 +0300

    Update Problema.8.py

--- Problema.8.py
+++ Problema.8.py
@@ -1,10 +1,15 @@
-S='A B a Z Z Y c'
+n=int(input('Introduceti numarul de siruri:'))
+S=[str(input('Introduceti literele sirului: ')) for i in range(n)]
+print(S)
 S1=''
-for i in range(len(S)):
-    if ord(S[i])==90:
-        S1+='A'
-    elif ord(S[i])==32:
-        S1+=' '
-    else:
-        S1+=chr(ord(S[i])+1)
-print('Sirul modificat este', S1)
+print('Sirurile sunt:')
+for i in S:
+    for j in range(len(i)):
+        if ord(i[j])==90:
+            S1+='A'
+        elif ord(i[j])==32:
+            S1+='-'
+        else:
+            S1+=chr(ord(i[j])+1)
+    print(S1) 
+    S1=''
