b85d17c89ade19cca8d2b7a1c6380d7ad17aa00b
elenamoglan <70642076+elenamoglan@users.noreply.github.com>
2021-10-19 21:49:48 +0300

    Update Problema.8.py

--- Problema.8.py
+++ Problema.8.py
@@ -1,14 +1,13 @@
 n=int(input('Introduceti numarul de siruri:'))
 S=[str(input('Introduceti literele sirului: ')) for i in range(n)]
-S1=''
 print('Sirurile sunt:')
 for i in S:
+    S1=''
     for j in range(len(i)):
-        if ord(i[j])==90:
+        if i[j]=='Z':
             S1+='A'
-        elif ord(i[j])==32:
+        elif i[j]==' ':
             S1+='-'
         else:
             S1+=chr(ord(i[j])+1)
-    print(S1) 
-    S1=''
+    print(S1)
